42806fb3557016b8b40ce6ea447ffeed5ad22c64
Ankit <115006624+ANKIT638-ux@users.noreply.github.com>
2023-08-20 17:26:34 +0530

    create paper model

--- models/paper.js
+++ models/paper.js
@@ -5,7 +5,7 @@ mongooseLong(connector);
 const { Long } = connector.Schema.Types;
 
 const paperSchema = {
-  answerSheetID: { type: Long, required: true },
+  answerSheetID: { type: Long.Long, required: true },
   exam: [{ type: connector.Schema.Types.ObjectId, ref: "Exam", required: true }],
   student: [{ type: connector.Schema.Types.ObjectId, ref: "Student", required: true }],
   checkedBy: [{ type: connector.Schema.Types.ObjectId, ref: "Faculty", required: true }],
