63cf35d5afbff9f2f6e423f9e9da5fed2d6bc076
Vighnesh Mishra <vighnesh97531@gmail.com>
2023-08-22 20:15:51 +0530

    [Added] Model for Activity BluePrint

---
+++ models/activityBlueprint.js
@@ -0,0 +1,18 @@
+import connector from "#models/databaseUtil"
+
+const activityBluePrintSchema = {
+    number : { type: Number, required: true },
+    academicYearYear: {
+        type: String,
+        required: true,
+        validate: {
+          validator: function(value) {
+            return /^2\d{3}$/.test(value); // Matches "2" followed by any 3 digits
+          },
+          message: props => `${props.value} is not a valid year format starting with "2"!`
+        }
+      },
+      type : { enum : [Odd, even] , required : true},
+    startDate: {type : Date, required : true},
+    endDate : {type : Date, required : true}
+}
