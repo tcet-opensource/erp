d9e08710a71296083649d6b021d1bd3f607f3859
Abhishek Goyal <abhishekgoyal1311@gmail.com>
2023-08-23 16:47:56 +0530

    Created a model for exam

--- models/exam.js
+++ models/exam.js
@@ -2,10 +2,10 @@ import connector from "#models/databaseUtil";
 
 const examSchema = {
   date: { type: Date, required: true },
-  startTime: { type: time, required: true },
+  startTime: { type: Date, required: true },
   duration: { type: Number, required: true },
   supervisor: { type: connector.Schema.Types.ObjectId, ref: "Faculty", required: "true" },
-  Infrastructure: { type: connector.Schema.Types.ObjectId, ref: "infrastructure", required: "true" },
+  infrastructure: { type: connector.Schema.Types.ObjectId, ref: "Infrastructure", required: "true" },
   course: { type: connector.Schema.Types.ObjectId, ref: "Course", required: "true" },
 };
 
