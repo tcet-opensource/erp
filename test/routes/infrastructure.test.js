175943b00cfb1418cc20069d59eec2711941b600
mouryasujit <mouryasujit04@gmail.com>
2023-08-24 23:54:49 +0530

    Adding the changes

--- test/routes/infrastructure.test.js
+++ test/routes/infrastructure.test.js
@@ -1,65 +1,132 @@
-import { addinfrastructure } from '../controllers/infrastructure';
+import mongoose from 'mongoose';
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import { jest } from '@jest/globals';
+import databaseUtil from '#models/databaseUtil';
+import app from '#app';
 
-describe('Infrastructure Controller', () => {
-  const mockInfrastructure = {
-    name: 'Building A',
-    type: 'Office',
-    wing: 'East',
-    floor: 3,
-    capacity: 100,
-  };
+import InfrastructureFunctions from '#models/infrastructure';
 
-  it('should add infrastructure', async () => {
-    const createSpy = jest.spyOn(addinfrastructure, 'create');
-    createSpy.mockResolvedValue(mockInfrastructure);
+const request = require('supertest');
+jest.mock('#util');
+let mongoServer;
+let server;
+let agent;
+beforeAll((done) => {
+  server = app.listen(5000, () => {
+    agent = request.agent(server);
+    connector.set('debug', false);
+    done();
+  });
+});
 
-    const result = await addinfrastructure.addInfrastructure(
-      mockInfrastructure
+function cleanUp(callback) {
+  accreditationModel.remove({ name: 'xyz' }).then(() => {
+    connector.disconnect((DBerr) => {
+      if (DBerr) console.log('Database dissconnnect error: ', DBerr);
+      server.close((serverErr) => {
+        if (serverErr) console.log(serverErr);
+        callback();
+      });
+    });
+  });
+}
+
+afterAll((done) => {
+  cleanUp(done);
+});
+
+describe('Infrastructure API Endpoints', () => {
+  // ... (beforeEach, beforeAll, afterAll, etc.)
+
+  test('Read Infrastructure', async () => {
+    await InfrastructureFunctions.create(
+      'Building A',
+      'Office',
+      'East Wing',
+      2,
+      100
     );
-    expect(result).toEqual(mockInfrastructure);
-    expect(createSpy).toHaveBeenCalledWith(
-      mockInfrastructure.name,
-      mockInfrastructure.type,
-      mockInfrastructure.wing,
-      mockInfrastructure.floor,
-      mockInfrastructure.capacity
+    await InfrastructureFunctions.create(
+      'Building B',
+      'Residential',
+      'West Wing',
+      1,
+      50
     );
+
+    const response = await request(app).get('/api/infrastructures'); // Adjust the API endpoint
+
+    expect(response.status).toBe(200);
+    expect(response.body).toHaveLength(2);
+    expect(response.body[0].name).toBe('Building A');
+    expect(response.body[1].name).toBe('Building B');
   });
 
-  it('should read infrastructure', async () => {
-    const filter = { name: 'Building A' };
-    const limit = 5;
-    const readSpy = jest.spyOn(addinfrastructure, 'read');
-    readSpy.mockResolvedValue([mockInfrastructure]);
-
-    const result = await addinfrastructure.readInfrastructure(filter, limit);
-    expect(result).toEqual([mockInfrastructure]);
-    expect(readSpy).toHaveBeenCalledWith(filter, limit);
-  });
-
-  it('should update infrastructure', async () => {
-    const filter = { name: 'Building A' };
-    const updateData = { capacity: 150 };
-    const updatedInfrastructure = { ...mockInfrastructure, capacity: 150 };
-    const updateSpy = jest.spyOn(addinfrastructure, 'update');
-    updateSpy.mockResolvedValue(updatedInfrastructure);
-
-    const result = await addinfrastructure.updateInfrastructure(
-      filter,
-      updateData
+  test('Update Infrastructure', async () => {
+    const createdInfrastructure = await InfrastructureFunctions.create(
+      'Building A',
+      'Office',
+      'East Wing',
+      2,
+      100
     );
-    expect(result).toEqual(updatedInfrastructure);
-    expect(updateSpy).toHaveBeenCalledWith(filter, updateData);
+
+    const response = await request(app)
+      .put(`/api/infrastructures/${createdInfrastructure._id}`) // Adjust the API endpoint
+      .send({ capacity: 150 });
+
+    expect(response.status).toBe(200);
+    expect(response.body.capacity).toBe(150);
   });
 
-  it('should remove infrastructure', async () => {
-    const filter = { name: 'Building A' };
-    const removedInfrastructure = { ...mockInfrastructure };
-    const removeSpy = jest.spyOn(addinfrastructure, 'remove');
-    removeSpy.mockResolvedValue(removedInfrastructure);
+  test('Remove Infrastructure', async () => {
+    const createdInfrastructure = await InfrastructureFunctions.create(
+      'Building A',
+      'Office',
+      'East Wing',
+      2,
+      100
+    );
 
-    const result = await addinfrastructure.removeInfrastructure(filter);
-    expect(result).toEqual(removedInfrastructure);
-    expect(removeSpy).toHaveBeenCalledWith(filter);
+    const response = await request(app).delete(
+      `/api/infrastructures/${createdInfrastructure._id}`
+    ); // Adjust the API endpoint
+
+    expect(response.status).toBe(200);
+    expect(response.body).toMatchObject({
+      name: 'Building A',
+      type: 'Office',
+      wing: 'East Wing',
+      floor: 2,
+      capacity: 100,
+    });
+
+    const infrastructuresResponse = await request(app).get(
+      '/api/infrastructures'
+    );
+    expect(infrastructuresResponse.body).toHaveLength(0);
+  });
+
+  test('Get Infrastructure by ID', async () => {
+    const createdInfrastructure = await InfrastructureFunctions.create(
+      'Building A',
+      'Office',
+      'East Wing',
+      2,
+      100
+    );
+
+    const response = await request(app).get(
+      `/api/infrastructures/${createdInfrastructure._id}`
+    ); // Adjust the API endpoint
+
+    expect(response.status).toBe(200);
+    expect(response.body).toMatchObject({
+      name: 'Building A',
+      type: 'Office',
+      wing: 'East Wing',
+      floor: 2,
+      capacity: 100,
+    });
   });
 });
