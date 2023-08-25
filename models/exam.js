c870c8ab3cc3e0b7c295f50faeac73bab63e4e4d
Astha Singh <singhastha182004@gmail.com>
2023-08-25 15:52:48 +0530

    #218-curd-to-exam-model

--- models/exam.js
+++ models/exam.js
@@ -11,34 +11,30 @@ const examSchema = {
 
 const Exam = connector.model("Exam", examSchema);
 
-async function remove(filter) {
-  const res = await Exam.findOneAndDelete(filter);
-  return res;
-}
-
-async function create(date, startTime, duration, supervisor, infrastructure, course) {
-  const exam = new Exam({
-    date,
-    startTime,
-    duration,
-    supervisor,
-    infrastructure,
-    course,
-  });
+async function create(examData) {
+  const exam = new Exam(examData);
   const examDoc = await exam.save();
   return examDoc;
 }
 
 async function read(filter, limit = 1) {
-  const examData = await Exam.find(filter).limit(limit);
-  return examData;
+  const examDoc = await Exam.find(filter).limit(limit);
+  return examDoc;
 }
 
-async function update(filter, updateObject) {
-  const exam = await Exam.findOneAndUpdate(filter, updateObject, { new: true });
-  return exam;
+async function update(filter, updateObject, options = { multi: true }) {
+  const updateResult = await Exam.updateMany(filter, { $set: updateObject }, options);
+  return updateResult.acknowledged;
+}
+
+async function remove(filter) {
+  const deleteResult = await Exam.deleteMany(filter).exec();
+  return deleteResult.acknowledged;
 }
 
 export default {
-  create, read, update, remove,
+  create,
+  read,
+  update,
+  remove,
 };
