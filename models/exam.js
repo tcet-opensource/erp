ad08cf66ebb4ce5af92310a91361582c76b0aea8
Astha Singh <singhastha182004@gmail.com>
2023-08-25 14:45:38 +0530

    #218-curd-to-exam-model

--- models/exam.js
+++ models/exam.js
@@ -9,4 +9,36 @@ const examSchema = {
   course: { type: connector.Schema.Types.ObjectId, ref: "Course", required: "true" },
 };
 
-const exam = connector.model("Exam", examSchema);
+const Exam = connector.model("Exam", examSchema);
+
+async function remove(filter) {
+  const res = await Exam.findOneAndDelete(filter);
+  return res;
+}
+
+async function create(date, startTime, duration, supervisor, infrastructure, course) {
+  const exam = new Exam({
+    date,
+    startTime,
+    duration,
+    supervisor,
+    infrastructure,
+    course,
+  });
+  const examDoc = await exam.save();
+  return examDoc;
+}
+
+async function read(filter, limit = 1) {
+  const examData = await Exam.find(filter).limit(limit);
+  return examData;
+}
+
+async function update(filter, updateObject) {
+  const exam = await Exam.findOneAndUpdate(filter, updateObject, { new: true });
+  return exam;
+}
+
+export default {
+  create, read, update, remove,
+};
