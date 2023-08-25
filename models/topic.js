1d2c7fdc821f84ea5138ed2441a4730657b743a5
Rahul <rahulsinghhh2312@gmail.com>
2023-08-25 10:45:25 +0530

    Update topic.js

--- models/topic.js
+++ models/topic.js
@@ -1,7 +1,7 @@
 import connector from "#models/databaseUtil";
-// eslint-disable-next-line  no-unused-vars
+
 const topicSchema = {
   title: { type: String, required: true },
 };
-
+// eslint-disable-next-line  no-unused-vars
 const Topic = connector.model("topic", topicSchema);
