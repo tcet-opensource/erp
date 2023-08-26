50d875f96566ecacd39724104433f92808b1f944
mouryasujit <mouryasujit04@gmail.com>
2023-08-26 21:38:12 +0530

    Adding the test cases for infrastructure endpoint

--- test/routes/infrastructure.test.js
+++ test/routes/infrastructure.test.js
@@ -1,16 +1,14 @@
-import mongoose from 'mongoose';
-import { MongoMemoryServer } from 'mongodb-memory-server';
 import { jest } from '@jest/globals';
-import databaseUtil from '#models/databaseUtil';
-import app from '#app';
+import request from 'supertest';
+import app from '#app'; // Update this import based on your app's structure
+import connector from '#models/databaseUtil'; // Update this import
+import infrastructureModel from '#models/infrastructure'; // Update this import
 
-import InfrastructureFunctions from '#models/infrastructure';
-
-const request = require('supertest');
 jest.mock('#util');
-let mongoServer;
+
 let server;
 let agent;
+
 beforeAll((done) => {
   server = app.listen(5000, () => {
     agent = request.agent(server);
@@ -20,113 +18,78 @@ beforeAll((done) => {
 });
 
 function cleanUp(callback) {
-  accreditationModel.remove({ name: 'xyz' }).then(() => {
-    connector.disconnect((DBerr) => {
-      if (DBerr) console.log('Database dissconnnect error: ', DBerr);
-      server.close((serverErr) => {
-        if (serverErr) console.log(serverErr);
-        callback();
+  infrastructureModel
+    .remove({
+      name: 'Building A',
+      type: 'Office',
+      wing: 'East',
+      floor: 3,
+      capacity: 100,
+    })
+    .then(() => {
+      connector.disconnect((DBerr) => {
+        if (DBerr) console.log('Database disconnect error: ', DBerr);
+        server.close((serverErr) => {
+          if (serverErr) console.log(serverErr);
+          callback();
+        });
       });
     });
-  });
 }
 
 afterAll((done) => {
   cleanUp(done);
 });
 
-describe('Infrastructure API Endpoints', () => {
-  // ... (beforeEach, beforeAll, afterAll, etc.)
-
-  test('Read Infrastructure', async () => {
-    await InfrastructureFunctions.create(
-      'Building A',
-      'Office',
-      'East Wing',
-      2,
-      100
-    );
-    await InfrastructureFunctions.create(
-      'Building B',
-      'Residential',
-      'West Wing',
-      1,
-      50
-    );
-
-    const response = await request(app).get('/api/infrastructures'); // Adjust the API endpoint
-
-    expect(response.status).toBe(200);
-    expect(response.body).toHaveLength(2);
-    expect(response.body[0].name).toBe('Building A');
-    expect(response.body[1].name).toBe('Building B');
-  });
-
-  test('Update Infrastructure', async () => {
-    const createdInfrastructure = await InfrastructureFunctions.create(
-      'Building A',
-      'Office',
-      'East Wing',
-      2,
-      100
-    );
-
-    const response = await request(app)
-      .put(`/api/infrastructures/${createdInfrastructure._id}`) // Adjust the API endpoint
-      .send({ capacity: 150 });
-
-    expect(response.status).toBe(200);
-    expect(response.body.capacity).toBe(150);
-  });
-
-  test('Remove Infrastructure', async () => {
-    const createdInfrastructure = await InfrastructureFunctions.create(
-      'Building A',
-      'Office',
-      'East Wing',
-      2,
-      100
-    );
-
-    const response = await request(app).delete(
-      `/api/infrastructures/${createdInfrastructure._id}`
-    ); // Adjust the API endpoint
-
-    expect(response.status).toBe(200);
-    expect(response.body).toMatchObject({
+describe('Infrastructure API', () => {
+  it('should create infrastructure', async () => {
+    const response = await agent.post('/infrastructure/add').send({
       name: 'Building A',
       type: 'Office',
-      wing: 'East Wing',
-      floor: 2,
+      wing: 'East',
+      floor: 3,
       capacity: 100,
     });
 
-    const infrastructuresResponse = await request(app).get(
-      '/api/infrastructures'
-    );
-    expect(infrastructuresResponse.body).toHaveLength(0);
+    expect(response.status).toBe(200);
+    expect(response.body.res).toMatch(/added user/);
   });
 
-  test('Get Infrastructure by ID', async () => {
-    const createdInfrastructure = await InfrastructureFunctions.create(
-      'Building A',
-      'Office',
-      'East Wing',
-      2,
-      100
-    );
+  describe('after adding infrastructure', () => {
+    beforeEach(async () => {
+      await agent.post('/infrastructure/add').send({
+        name: 'Building A',
+        type: 'Office',
+        wing: 'East',
+        floor: 3,
+        capacity: 100,
+      });
+    });
 
-    const response = await request(app).get(
-      `/api/infrastructures/${createdInfrastructure._id}`
-    ); // Adjust the API endpoint
+    afterEach(async () => {
+      await infrastructureModel.remove({
+        name: 'Building A',
+        type: 'Office',
+        wing: 'East',
+        floor: 3,
+        capacity: 100,
+      });
+    });
 
-    expect(response.status).toBe(200);
-    expect(response.body).toMatchObject({
-      name: 'Building A',
-      type: 'Office',
-      wing: 'East Wing',
-      floor: 2,
-      capacity: 100,
+    it('should read infrastructure', async () => {
+      const response = await agent
+        .post('/infrastructure/list')
+        .send({ name: 'Building A' });
+      expect(response.body.res).not.toBeNull();
+    });
+
+    it('should update infrastructure', async () => {
+      const response = await agent
+        .post('/infrastructure/update')
+        .send({ name: 'Building A' }, { capacity: 150 });
+
+      expect(response.status).toBe(200);
+      expect(response.body.res).toMatch(/updated infrastructure/);
     });
   });
 });
