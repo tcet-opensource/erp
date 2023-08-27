c876e2e545030d078bf21f8d85e7d06420106c10
Tejas Nair <85873779+TejasNair9977@users.noreply.github.com>
2023-08-27 11:52:45 +0530

    eslint fixes

--- test/routes/infrastructure.test.js
+++ test/routes/infrastructure.test.js
@@ -1,10 +1,10 @@
-import { jest } from '@jest/globals';
-import request from 'supertest';
-import app from '#app'; // Update this import based on your app's structure
-import connector from '#models/databaseUtil'; // Update this import
-import infrastructureModel from '#models/infrastructure'; // Update this import
+import { jest } from "@jest/globals"; // eslint-disable-line import/no-extraneous-dependencies
+import request from "supertest";
+import app from "#app"; // Update this import based on your app"s structure
+import connector from "#models/databaseUtil"; // Update this import
+import infrastructureModel from "#models/infrastructure"; // Update this import
 
-jest.mock('#util');
+jest.mock("#util");
 
 let server;
 let agent;
@@ -12,7 +12,7 @@ let agent;
 beforeAll((done) => {
   server = app.listen(5000, () => {
     agent = request.agent(server);
-    connector.set('debug', false);
+    connector.set("debug", false);
     done();
   });
 });
@@ -20,15 +20,15 @@ beforeAll((done) => {
 function cleanUp(callback) {
   infrastructureModel
     .remove({
-      name: 'Building A',
-      type: 'Office',
-      wing: 'East',
+      name: "Building A",
+      type: "Office",
+      wing: "East",
       floor: 3,
       capacity: 100,
     })
     .then(() => {
       connector.disconnect((DBerr) => {
-        if (DBerr) console.log('Database disconnect error: ', DBerr);
+        if (DBerr) console.log("Database disconnect error: ", DBerr);
         server.close((serverErr) => {
           if (serverErr) console.log(serverErr);
           callback();
@@ -41,12 +41,12 @@ afterAll((done) => {
   cleanUp(done);
 });
 
-describe('Infrastructure API', () => {
-  it('should create infrastructure', async () => {
-    const response = await agent.post('/infrastructure/add').send({
-      name: 'Building A',
-      type: 'Office',
-      wing: 'East',
+describe("Infrastructure API", () => {
+  it("should create infrastructure", async () => {
+    const response = await agent.post("/infrastructure/add").send({
+      name: "Building A",
+      type: "Office",
+      wing: "East",
       floor: 3,
       capacity: 100,
     });
@@ -55,12 +55,12 @@ describe('Infrastructure API', () => {
     expect(response.body.res).toMatch(/added user/);
   });
 
-  describe('after adding infrastructure', () => {
+  describe("after adding infrastructure", () => {
     beforeEach(async () => {
-      await agent.post('/infrastructure/add').send({
-        name: 'Building A',
-        type: 'Office',
-        wing: 'East',
+      await agent.post("/infrastructure/add").send({
+        name: "Building A",
+        type: "Office",
+        wing: "East",
         floor: 3,
         capacity: 100,
       });
@@ -68,25 +68,25 @@ describe('Infrastructure API', () => {
 
     afterEach(async () => {
       await infrastructureModel.remove({
-        name: 'Building A',
-        type: 'Office',
-        wing: 'East',
+        name: "Building A",
+        type: "Office",
+        wing: "East",
         floor: 3,
         capacity: 100,
       });
     });
 
-    it('should read infrastructure', async () => {
+    it("should read infrastructure", async () => {
       const response = await agent
-        .post('/infrastructure/list')
-        .send({ name: 'Building A' });
+        .post("/infrastructure/list")
+        .send({ name: "Building A" });
       expect(response.body.res).not.toBeNull();
     });
 
-    it('should update infrastructure', async () => {
+    it("should update infrastructure", async () => {
       const response = await agent
-        .post('/infrastructure/update')
-        .send({ name: 'Building A' }, { capacity: 150 });
+        .post("/infrastructure/update")
+        .send({ name: "Building A" }, { capacity: 150 });
 
       expect(response.status).toBe(200);
       expect(response.body.res).toMatch(/updated infrastructure/);
