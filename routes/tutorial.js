ec382aa9fc7c267cac4d0a2e1fc978df14a8e772
Astha Singh <singhastha182004@gmail.com>
2023-09-01 16:03:14 +0530

    #273-endpoints-for-tutorials

---
+++ routes/tutorial.js
@@ -0,0 +1,10 @@
+import express from "express";
+import tutorialController from "#controller/tutorial";
+
+const router = express.Router();
+router.post("/add", tutorialController.addTutorial);
+router.get("/list", tutorialController.getTutorial);
+router.post("/update", tutorialController.updateTutorial);
+router.post("/delete", tutorialController.deleteTutorial);
+
+export default router;
---
+++ services/tutorial.js
@@ -0,0 +1,43 @@
+import Tutorial from "#models/tutorial";
+import databaseError from "#error/database";
+
+export async function addNewTutorial(no, title, hours, cognitiveLevel,) {
+  const newTutorial = await Tutorial.create({
+    no,
+    title,
+    hours,
+    cognitiveLevel,
+  });
+  if (newTutorial.name === name) {
+    return newTutorial;
+  }
+  throw new databaseError.DataEntryError("Add Tutorial");
+}
+
+export async function gettutorials(filter) {
+  const tutorials = await Tutorial.read(filter);
+  if (tutorials) {
+    return tutorials;
+  }
+  throw new databaseError.DataNotFoundError("Tutorial");
+}
+
+export async function deleteTutorialById(tutorialId) {
+  const deleted = await Tutorial.remove({ _id: tutorialId });
+  if (deleted) {
+    return deleted;
+  }
+  throw new databaseError.DataDeleteError("Tutorial");
+}
+
+export async function updateTutorialById(id, data) {
+  const updated = await Tutorial.update({ _id: id }, data);
+  if (updated) {
+    return updated;
+  }
+  throw new databaseError.DataEntryError("Tutorial");
+}
+
+export default {
+  deleteTutorialById, addNewTutorial, updateTutorialById,
+};
