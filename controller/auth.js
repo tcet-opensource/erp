3e737cadcebb839544f9f43afae6d56a4322965c
shreyam462 <shreya.m462@gmail.com>
2023-09-02 19:14:05 +0530

    [Bug]changed res for validateUser API

--- controller/auth.js
+++ controller/auth.js
@@ -28,7 +28,12 @@ async function login(req, res) {
 }
 
 function validateUser(req, res) {
+  if(req.user){
   res.json({ res: req.user, msg: "user validated", err: null });
+}
+else{
+  res.status(401).json({res:null, msg:"unauthorised", err:"User not authorised"})
+}
 }
 
 async function sendOTP(req, res) {
