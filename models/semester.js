f58012edf63d7801e3c987cd8f99b0962cd5f983
Deepti Singh <136986628+deepti-50@users.noreply.github.com>
2023-09-08 12:32:11 +0530

    [UPDATED]updated curd operation in semester model
    fixes #282 
    updated curd operation in semester.js in models folder

--- models/semester.js
+++ models/semester.js
@@ -1,6 +1,6 @@
 import connector from "#models/databaseUtil";
 const semesterSchema = {
-  number:{Number,enum: [1,2,3,4,5,6,7,8], required:true},
+  number:{ type: Number,enum: [1,2,3,4,5,6,7,8], required:true},
   academicYear: {
     type: String,
     required: true,
