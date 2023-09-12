8c4eb43a0e67fcf823b160892c84ca1a34c03cf4
sumitst05 <sumitst05@gmail.com>
2023-09-12 19:25:17 +0530

    created testcases for all endpoints

---
+++ test/routes/group.test.js
@@ -0,0 +1,86 @@
+import { jest } from "@jest/globals"; // eslint-disable-line import/no-extraneous-dependencies
+import request from "supertest";
+import app from "#app";
+import connector from "#models/databaseUtil";
+import groupModel from "#models/group";
+
+jest.mock("#util");
+
+let server;
+let agent;
+
+beforeAll((done) => {
+  server = app.listen(null, () => {
+    agent = request.agent(server);
+    connector.set("debug", false);
+    done();
+  });
+});
+
+function cleanUp(callback) {
+  groupModel
+    .remove({
+      title: "Group 1",
+      student: "64fdc67feca8a69f01b33614",
+    })
+    .then(() => {
+      connector.disconnect((DBerr) => {
+        if (DBerr) console.log("Database disconnect error: ", DBerr);
+        server.close((serverErr) => {
+          if (serverErr) console.log(serverErr);
+          callback();
+        });
+      });
+    });
+}
+
+afterAll((done) => {
+  cleanUp(done);
+});
+
+describe("group API", () => {
+  it("should create group", async () => {
+    const response = await agent.post("/group/add").send({
+      title: "Group 1",
+      student: "64fdc67feca8a69f01b33614",
+    });
+
+    expect(response.status).toBe(200);
+    expect(response.body.res).toMatch(/added group/);
+  });
+
+  describe("after adding group", () => {
+    let id;
+    beforeEach(async () => {
+      id = await agent.post("/group/add").send({
+        title: "Group 1",
+        student: "64fdc67feca8a69f01b33614",
+      });
+      id = JSON.parse(id.res.text).id;
+    });
+
+    afterEach(async () => {
+      await groupModel.remove({
+        title: "Group 1",
+        student: "64fdc67feca8a69f01b33614",
+      });
+    });
+
+    it("should read group", async () => {
+      const response = await agent
+        .get("/group/list")
+        .send({ name: "Building A" });
+      expect(response.status).toBe(200);
+      expect(response.body.res).toBeDefined();
+    });
+
+    it("should update group", async () => {
+      const response = await agent
+        .post(`/group/update/${id}`)
+        .send({ title: "Group 1" }, { title: "Group 2" });
+
+      expect(response.status).toBe(200);
+      expect(response.body.res).toMatch(/updated group/);
+    });
+  });
+});
