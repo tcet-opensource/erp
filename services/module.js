28f6a6039c6e64e5ba6589d18081522b93dd3e5f
Abhishek Goyal <abhishekgoyal1311@gmail.com>
2023-09-20 16:44:09 +0530

    262-Created-endpoint-for-Module

--- services/module.js
+++ services/module.js
@@ -18,3 +18,19 @@ export async function addNewModule(no, name, outcome, contents, hrsPerModule, co
   }
   throw new databaseError.DataEntryError("Add Module");
 }
+
+export async function updateModuleById(id,data) {
+  const updated = await Module.update({_id: id}, data);
+  if (updated) {
+    return updated;
+  }
+  throw new databaseError.DataEntryError("Module")
+}
+
+export async function deleteModuleById(ModuleId) {
+  const deleted = await Module.remove({ _id: ModuleId });
+  if (deleted) {
+    return deleted;
+  }
+  throw new databaseError.DataDeleteError("Module")
+}
