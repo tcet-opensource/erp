f1e8e9d675af6fda5daa9195cbc4f48a05ec253e
Hricha Mehra <hrichamehra11@gmail.com>
2023-10-10 22:56:27 +0530

    created end points for topic

--- app.js
+++ app.js
@@ -25,6 +25,7 @@ import { identifyUser } from "#middleware/identifyUser";
 import departmentRouter from "#routes/department";
 import paperRouter from "#routes/paper";
 import groupRouter from "#routes/group";
+import topicRouter from "#routes/topic";
 
 const app = express();
 const currDirName = dirname(fileURLToPath(import.meta.url));
@@ -62,5 +63,6 @@ app.use("/paper", paperRouter);
 app.use("/group", groupRouter);
 app.use("/semester", semesterRouter);
 app.use("/faculty", facultyRouter);
+app.use("/topic", topicRouter);
 
 export default app;
