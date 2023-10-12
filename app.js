daff42cba388a1e279b112869ff0bf149615af47
Dhruv <dhruvrg2003@gmail.com>
2023-10-12 17:03:17 +0530

    eslint changes in app.js

--- app.js
+++ app.js
@@ -1,31 +1,31 @@
-import path, { dirname } from 'path';
-import { fileURLToPath } from 'url';
-import express from 'express';
-import morgan from 'morgan';
-import cookieParser from 'cookie-parser';
-import cors from 'cors';
-import { logger } from '#util';
-import indexRouter from '#routes/index';
-import usersRouter from '#routes/users';
-import authRouter from '#routes/auth';
-import accreditationRouter from '#routes/accreditation';
-import infrastructureRouter from '#routes/infrastructure';
-import practicalRouter from '#routes/practical';
-import organizationRouter from '#routes/organization';
-import studentRouter from '#routes/student';
-import tutorialRouter from '#routes/tutorial';
-import assignmentRouter from '#routes/assignment';
-import timetableRouter from '#routes/timetable';
-import courseworkRouter from '#routes/coursework';
-import moduleRouter from '#routes/module';
-import { identifyUser } from '#middleware/identifyUser';
-import departmentRouter from '#routes/department';
-import examRouter from '#routes/exam';
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
+import express from "express";
+import morgan from "morgan";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import { logger } from "#util";
+import indexRouter from "#routes/index";
+import usersRouter from "#routes/users";
+import authRouter from "#routes/auth";
+import accreditationRouter from "#routes/accreditation";
+import infrastructureRouter from "#routes/infrastructure";
+import practicalRouter from "#routes/practical";
+import organizationRouter from "#routes/organization";
+import studentRouter from "#routes/student";
+import tutorialRouter from "#routes/tutorial";
+import assignmentRouter from "#routes/assignment";
+import timetableRouter from "#routes/timetable";
+import courseworkRouter from "#routes/coursework";
+import moduleRouter from "#routes/module";
+import { identifyUser } from "#middleware/identifyUser";
+import departmentRouter from "#routes/department";
+import examRouter from "#routes/exam";
 
 const app = express();
 const currDirName = dirname(fileURLToPath(import.meta.url));
 
-morgan.token('remote-user', (req) => req.user);
+morgan.token("remote-user", (req) => req.user);
 app.use(identifyUser);
 app.use(cors());
 app.use(express.json());
@@ -33,28 +33,28 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(
   morgan(
-    ':remote-addr - :remote-user ":method :url HTTP/:http-version" :status ":referrer" ":user-agent"',
-    { stream: logger.stream }
-  )
+    ":remote-addr - :remote-user \":method :url HTTP/:http-version\" :status \":referrer\" \":user-agent\"",
+    { stream: logger.stream },
+  ),
 );
 
-app.use(express.static(path.join(currDirName, 'public')));
+app.use(express.static(path.join(currDirName, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/auth', authRouter);
-app.use('/accreditation', accreditationRouter);
-app.use('/infrastructure', infrastructureRouter);
-app.use('/department', departmentRouter);
-app.use('/practical', practicalRouter);
-app.use('/organization', organizationRouter);
-app.use('/student', studentRouter);
-app.use('/tutorial', tutorialRouter);
-app.use('/assignment', assignmentRouter);
-app.use('/timetable', timetableRouter);
-app.use('/department', departmentRouter);
-app.use('/coursework', courseworkRouter);
-app.use('/module', moduleRouter);
-app.use('/exam', examRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/auth", authRouter);
+app.use("/accreditation", accreditationRouter);
+app.use("/infrastructure", infrastructureRouter);
+app.use("/department", departmentRouter);
+app.use("/practical", practicalRouter);
+app.use("/organization", organizationRouter);
+app.use("/student", studentRouter);
+app.use("/tutorial", tutorialRouter);
+app.use("/assignment", assignmentRouter);
+app.use("/timetable", timetableRouter);
+app.use("/department", departmentRouter);
+app.use("/coursework", courseworkRouter);
+app.use("/module", moduleRouter);
+app.use("/exam", examRouter);
 
 export default app;
